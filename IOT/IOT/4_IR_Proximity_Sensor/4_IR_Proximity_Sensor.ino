741de63902a59c89c11c786ad90cc2f5dda93540
Muthiah Prabhakaran <muthiahprabhakaran@gmail.com>
2020-09-01 19:30:08 +0530

    Proximity - received the input from the sensor

--- IOT/IOT/4_IR_Proximity_Sensor/4_IR_Proximity_Sensor.ino
+++ IOT/IOT/4_IR_Proximity_Sensor/4_IR_Proximity_Sensor.ino
@@ -1,9 +1,11 @@
 const int pin = 7; //LED connected to this pin
 const int ir = 4; //Out of IR is connected to this PIN
+int count = 0;
 void setup() {
   // put your setup code here, to run once:
   pinMode(pin,OUTPUT);
   pinMode(ir,INPUT); //We are going to take the input from IR sensor
+  Serial.begin(9600); //9600 bits per second
 }
 
 void loop() {
@@ -13,6 +15,8 @@ void loop() {
   if(val == LOW){
     // if(val == 0) also can be used
     digitalWrite(pin,HIGH);
+    Serial.println("There is someone at the door"+count);
+    count++;
     delay(1000);  
   }else{
     digitalWrite(pin,LOW);  
